d28cf33fa782bd8555786b95e873c77512b0052f
Runggaldier Samuel (Student Com14) <samuel.runggaldier@stud-inf.unibz.it>
2019-02-28 20:45:29 +0100

    Peter ist ein genie

--- ps/src/Main.java
+++ ps/src/Main.java
@@ -5,16 +5,45 @@ public class Main {
     public static void main (String[] args){
 
         ArrayList<Photo> photos = Util.readFile("b_lovely_landscapes.txt");
+        ArrayList<Photo> slideShow = new ArrayList<>();
 
-        for(Photo p : photos){
-            System.out.println(p);
+        int maxId = 0;
+
+        while (photos.size() > 0) {
+
+            Photo photo = photos.get(maxId);
+            photos.remove(maxId);
+            slideShow.add(photo);
+
+            int max = 0;
+            maxId = 0;
+
+            for (int i = 0; i < photos.size(); i++) {
+                Photo p1 = photo;
+                Photo p2 = photos.get(i);
+
+                int min = Util.calcInterestFactor(p1, p2);
+
+                if (min >= max) {
+                    max = min;
+                    maxId = i;
+                }
+            }
+
         }
 
+        for (Photo g: slideShow) {
+
+            System.out.println(g.id);
+
+        }
+
+        /*
         ArrayList<String> strings = new ArrayList<>();
         strings.add("A");
         strings.add("A");
 
-        Util.writeFile("test.txt", strings);
+        Util.writeFile("test.txt", strings);*/
 
     }
 }
